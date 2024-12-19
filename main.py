fc41f3cb289b16611a7e4305879ee0406a8496da
Rammohan <reddy.kambati@gmail.com>
2024-12-19 23:03:45 +0530

    added main.py

--- main.py
+++ main.py
@@ -14,15 +14,35 @@ class Contact:
 
 class AddressBook:
     def __init__(self):
-        self.contacts = []
+        self.contacts = {}
 
     def add_contact(self, first_name, last_name, address, city, state, zip_code, phone, email):
         new_contact = Contact(first_name, last_name, address, city, state, zip_code, phone, email)
-        self.contacts.append(new_contact)
+        self.contacts[first_name]=[new_contact]
 
+   
     def display_contacts(self):
-        for contact in self.contacts:
-            print(contact)
+     for name, contact_list in self.contacts.items():
+        print(f"'{name}': [")
+        for contact in contact_list:
+            print(f"  {contact}")
+        print("]")
+    
+     
+
 
 address_book = AddressBook()
 
+address_book.add_contact(
+    first_name=input("Enter the first name:"),
+    last_name=input("Enter the second name:"),
+    address=input("Enter the  address:"),
+    city=input("Enter the city name:"),
+    state=input("Enter the state name:"),
+    zip_code=input("Enter the pin code:"),
+    phone=input("Enter your Mobile number:"),
+    email=input("Enter your email:")
+)
+
+
+address_book.display_contacts()
